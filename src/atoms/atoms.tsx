0a3bbc1bd14620599c2e18eeeb2f36c197e5f707
tamani phiri <tamanigabriel0@gmail.com>
2024-02-06 21:31:25 +0200

    c

--- src/atoms/atoms.tsx
+++ src/atoms/atoms.tsx
@@ -1,6 +1,9 @@
 import { atom } from "recoil";
+import {recoilPersist} from 'recoil-persist'
 
 export const isAuthenticatedAtom = atom<boolean>({
   key: "isAuthenticatedState",
   default: false,
+  dangerouslyAllowMutability:true,
+  effects_UNSTABLE:[recoilPersist()]
 });
