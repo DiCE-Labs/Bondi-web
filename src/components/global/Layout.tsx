2052964ff8ba99174783dde1cd6989d822f5016f
tamani phiri <tamanigabriel0@gmail.com>
2024-02-06 22:34:38 +0200

    c

--- src/components/global/Layout.tsx
+++ src/components/global/Layout.tsx
@@ -15,7 +15,7 @@ const Layout: React.FC<Props> = ({ children }) => {
       backgroundColor={colorMode === "dark" ? "#02182B" : "white"}
     >
       <Navbar />
-      <Box as="main" className="min-h-screen w-full mt-24">
+      <Box as="main" className="min-h-screen w-full mt-20">
         {children}
       </Box>
     </Box>
