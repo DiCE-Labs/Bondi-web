ae731a2de7863c2572a2f152e869ef96fce4703e
tamani phiri <tamanigabriel0@gmail.com>
2024-02-06 22:37:52 +0200

    c

--- src/components/global/Navbar.tsx
+++ src/components/global/Navbar.tsx
@@ -1,14 +1,17 @@
 import { MoonIcon, SunIcon } from "@chakra-ui/icons";
 import { Button, Flex, IconButton, useColorMode } from "@chakra-ui/react";
+import { Link } from "react-router-dom";
 
 const Navbar = () => {
   const { toggleColorMode, colorMode } = useColorMode();
   return (
     <Flex
-      className={`w-full justify-between py-4 shadow-md fixed top-0 px-4 md:px-12`}
+      className={`w-full justify-between py-4 shadow-md fixed top-0 px-4 md:px-12 items-center`}
       backgroundColor={colorMode === "dark" ? "#02182B" : "white"}
     >
-      <Button>Nav</Button>
+      <Link to={"/"} className="font-bold">
+        Bondi
+      </Link>
       <IconButton
         aria-label="theme"
         onClick={() => toggleColorMode()}
