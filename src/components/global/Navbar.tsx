13dfaa4e18ba0ee9d032f8cdb4b90ad21b1fb0d0
tamani phiri <tamanigabriel0@gmail.com>
2024-02-06 22:45:48 +0200

    c

--- src/components/global/Navbar.tsx
+++ src/components/global/Navbar.tsx
@@ -1,9 +1,10 @@
 import { MoonIcon, SunIcon } from "@chakra-ui/icons";
 import { Button, Flex, IconButton, useColorMode } from "@chakra-ui/react";
-import { Link } from "react-router-dom";
+import { Link, NavLink, useLocation } from "react-router-dom";
 
 const Navbar = () => {
   const { toggleColorMode, colorMode } = useColorMode();
+  let location = useLocation();
   return (
     <Flex
       className={`w-full justify-between py-4 shadow-md fixed top-0 px-4 md:px-12 items-center`}
@@ -12,14 +13,43 @@ const Navbar = () => {
       <Link to={"/"} className="font-bold">
         Bondi
       </Link>
-      <IconButton
-        aria-label="theme"
-        onClick={() => toggleColorMode()}
-        icon={colorMode === "dark" ? <SunIcon /> : <MoonIcon />}
-        isRound
-      />
+
+      <Flex className=" gap-8 items-center">
+        {navLinks.map((l, i) => (
+          <NavLink
+            key={i}
+            to={l.link}
+            className={`${
+              location.pathname === l.link
+                ? colorMode === "dark"
+                  ? "text-[#A2FAA3]"
+                  : "text-[#1B98E0]"
+                : ""
+            }`}
+          >
+            {l.name}
+          </NavLink>
+        ))}
+        <IconButton
+          aria-label="theme"
+          onClick={() => toggleColorMode()}
+          icon={colorMode === "dark" ? <SunIcon /> : <MoonIcon />}
+          isRound
+        />
+      </Flex>
     </Flex>
   );
 };
 
 export default Navbar;
+
+const navLinks = [
+  {
+    name: "Home",
+    link: "/",
+  },
+  {
+    name: "Favorites",
+    link: "/favorites",
+  },
+];
