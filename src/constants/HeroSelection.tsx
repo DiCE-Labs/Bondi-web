6333f50313300b0316b49de44f54ee6f92f1f56c
tamani phiri <tamanigabriel0@gmail.com>
2024-02-10 23:54:13 +0200

    c

--- src/constants/HeroSelection.tsx
+++ src/constants/HeroSelection.tsx
@@ -1,14 +1,79 @@
-import { Box, useColorModeValue } from "@chakra-ui/react";
+import { ArrowDownIcon, ChevronDownIcon } from "@chakra-ui/icons";
+import {
+  Box,
+  Button,
+  Flex,
+  Menu,
+  MenuButton,
+  MenuItem,
+  MenuList,
+  Text,
+  useColorModeValue,
+} from "@chakra-ui/react";
 
 const HeroSelection = () => {
   return (
     <Box
-      className={`md:grid-cols-4 grid grid-cols-1 ${useColorModeValue(
+      className={`md:grid-cols-4 grid grid-cols-1 w-full ${useColorModeValue(
         "border-[#1B98E0]",
         "border-[#A2FAA3]"
       )} border-4`}
     >
-      1234
+      <Flex>
+        1{" "}
+        <Box>
+          <Text>2</Text>
+          <Menu>
+            <MenuButton as={Button} rightIcon={<ArrowDownIcon />}>
+              Actions
+            </MenuButton>
+            <MenuList>
+              <MenuItem>Download</MenuItem>
+              <MenuItem>Create a Copy</MenuItem>
+              <MenuItem>Mark as Draft</MenuItem>
+              <MenuItem>Delete</MenuItem>
+              <MenuItem>Attend a Workshop</MenuItem>
+            </MenuList>
+          </Menu>
+        </Box>
+      </Flex>
+      <Flex>
+        2{" "}
+        <Box>
+          <Text>2</Text>
+          <Menu>
+            <MenuButton as={Button} rightIcon={<ChevronDownIcon />}>
+              Actions
+            </MenuButton>
+            <MenuList>
+              <MenuItem>Download</MenuItem>
+              <MenuItem>Create a Copy</MenuItem>
+              <MenuItem>Mark as Draft</MenuItem>
+              <MenuItem>Delete</MenuItem>
+              <MenuItem>Attend a Workshop</MenuItem>
+            </MenuList>
+          </Menu>
+        </Box>
+      </Flex>
+      <Flex>
+        3{" "}
+        <Box>
+          <Text>2</Text>
+          <Menu>
+            <MenuButton as={Button} rightIcon={<ChevronDownIcon />}>
+              Actions
+            </MenuButton>
+            <MenuList>
+              <MenuItem>Download</MenuItem>
+              <MenuItem>Create a Copy</MenuItem>
+              <MenuItem>Mark as Draft</MenuItem>
+              <MenuItem>Delete</MenuItem>
+              <MenuItem>Attend a Workshop</MenuItem>
+            </MenuList>
+          </Menu>
+        </Box>
+      </Flex>
+      <Flex>4</Flex>
     </Box>
   );
 };
