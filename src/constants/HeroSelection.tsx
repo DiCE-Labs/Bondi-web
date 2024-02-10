9612e72c2cc6d02309b717bf48eb20aa7b0a54ef
tamani phiri <tamanigabriel0@gmail.com>
2024-02-11 00:06:52 +0200

    c

--- src/constants/HeroSelection.tsx
+++ src/constants/HeroSelection.tsx
@@ -4,13 +4,13 @@ import IconPin from "../assets/icons/PinIcon";
 const HeroSelection = () => {
   return (
     <Box
-      className={`md:grid-cols-4 grid grid-cols-1 p-4 mt-8 w-full ${useColorModeValue(
+      className={`md:grid-cols-4 grid grid-cols-1 rounded-md p-4 mt-8 w-full ${useColorModeValue(
         "border-[#1B98E0]",
         "border-[#A2FAA3]"
       )} border-4`}
     >
       <Flex>
-        <Box className="bg-gray-500">
+        <Box className="bg-gray-500 flex items-center justify-center p-2 rounded-md">
           <IconPin />
         </Box>
         <Box>
