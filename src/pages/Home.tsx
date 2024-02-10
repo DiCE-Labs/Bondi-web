80d444b9a0a591ff98541f241d895c40ef209f32
tamani phiri <tamanigabriel0@gmail.com>
2024-02-10 23:23:43 +0200

    c

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,11 +1,11 @@
-import { Box, Image, useColorModeValue } from "@chakra-ui/react";
+import { Box, Heading, Image, useColorModeValue } from "@chakra-ui/react";
 import bg from "../assets/hero.webp";
 
 const Home = () => {
   return (
     <Box as="header" className="grid grid-cols-1 lg:grid-cols-3 min-h-screen">
       <Box
-        className={`flex items-center justify-center w-full ${useColorModeValue(
+        className={`flex items-center lg:order-first order-last justify-center w-full ${useColorModeValue(
           "bg-[#1B98E0]",
           "bg-[#A2FAA3]"
         )}`}
@@ -18,7 +18,7 @@ const Home = () => {
         />
       </Box>
       <Box className=" lg:col-span-2 col-span-1">
-        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nobis ullam sapiente obcaecati asperiores sit non ea tenetur dolorum iusto ipsa hic, laborum nemo nisi placeat delectus cum et modi vel!
+        <Heading>We help people to realize their dream property</Heading>
       </Box>
     </Box>
   );
