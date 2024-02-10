f3dbbdf58e50ea64bf42e32b8ec43acc5b1f366f
tamani phiri <tamanigabriel0@gmail.com>
2024-02-11 00:05:40 +0200

    c

---
+++ src/assets/icons/PinIcon.tsx
@@ -0,0 +1,20 @@
+function IconPin() {
+  return (
+    <svg fill="none" viewBox="0 0 24 24" height="1em" width="1em">
+      <path
+        fill="currentColor"
+        fillRule="evenodd"
+        d="M16.272 10.272a4 4 0 11-8 0 4 4 0 018 0zm-2 0a2 2 0 11-4 0 2 2 0 014 0z"
+        clipRule="evenodd"
+      />
+      <path
+        fill="currentColor"
+        fillRule="evenodd"
+        d="M5.794 16.518a9 9 0 1112.724-.312l-6.206 6.518-6.518-6.206zm11.276-1.691l-4.827 5.07-5.07-4.827a7 7 0 119.897-.243z"
+        clipRule="evenodd"
+      />
+    </svg>
+  );
+}
+
+export default IconPin;
--- src/constants/HeroSelection.tsx
+++ src/constants/HeroSelection.tsx
@@ -1,76 +1,32 @@
-import { ArrowDownIcon, ChevronDownIcon } from "@chakra-ui/icons";
-import {
-  Box,
-  Button,
-  Flex,
-  Menu,
-  MenuButton,
-  MenuItem,
-  MenuList,
-  Text,
-  useColorModeValue,
-} from "@chakra-ui/react";
+import { Box, Flex, Text, useColorModeValue } from "@chakra-ui/react";
+import IconPin from "../assets/icons/PinIcon";
 
 const HeroSelection = () => {
   return (
     <Box
-      className={`md:grid-cols-4 grid grid-cols-1 w-full ${useColorModeValue(
+      className={`md:grid-cols-4 grid grid-cols-1 p-4 mt-8 w-full ${useColorModeValue(
         "border-[#1B98E0]",
         "border-[#A2FAA3]"
       )} border-4`}
     >
       <Flex>
-        1{" "}
+        <Box className="bg-gray-500">
+          <IconPin />
+        </Box>
         <Box>
-          <Text>2</Text>
-          <Menu>
-            <MenuButton as={Button} rightIcon={<ArrowDownIcon />}>
-              Actions
-            </MenuButton>
-            <MenuList>
-              <MenuItem>Download</MenuItem>
-              <MenuItem>Create a Copy</MenuItem>
-              <MenuItem>Mark as Draft</MenuItem>
-              <MenuItem>Delete</MenuItem>
-              <MenuItem>Attend a Workshop</MenuItem>
-            </MenuList>
-          </Menu>
+          <Text>Location</Text>
         </Box>
       </Flex>
       <Flex>
-        2{" "}
+        Price
         <Box>
-          <Text>2</Text>
-          <Menu>
-            <MenuButton as={Button} rightIcon={<ChevronDownIcon />}>
-              Actions
-            </MenuButton>
-            <MenuList>
-              <MenuItem>Download</MenuItem>
-              <MenuItem>Create a Copy</MenuItem>
-              <MenuItem>Mark as Draft</MenuItem>
-              <MenuItem>Delete</MenuItem>
-              <MenuItem>Attend a Workshop</MenuItem>
-            </MenuList>
-          </Menu>
+          <Text>Location</Text>
         </Box>
       </Flex>
       <Flex>
         3{" "}
         <Box>
           <Text>2</Text>
-          <Menu>
-            <MenuButton as={Button} rightIcon={<ChevronDownIcon />}>
-              Actions
-            </MenuButton>
-            <MenuList>
-              <MenuItem>Download</MenuItem>
-              <MenuItem>Create a Copy</MenuItem>
-              <MenuItem>Mark as Draft</MenuItem>
-              <MenuItem>Delete</MenuItem>
-              <MenuItem>Attend a Workshop</MenuItem>
-            </MenuList>
-          </Menu>
         </Box>
       </Flex>
       <Flex>4</Flex>
