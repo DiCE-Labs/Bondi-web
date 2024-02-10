1a636500e82eb494df0ae808f5532e67063dac91
tamani phiri <tamanigabriel0@gmail.com>
2024-02-11 00:09:06 +0200

    c

--- src/constants/HeroSelection.tsx
+++ src/constants/HeroSelection.tsx
@@ -1,4 +1,4 @@
-import { Box, Flex, Text, useColorModeValue } from "@chakra-ui/react";
+import { Box, Flex, Select, Text, useColorModeValue } from "@chakra-ui/react";
 import IconPin from "../assets/icons/PinIcon";
 
 const HeroSelection = () => {
@@ -15,6 +15,11 @@ const HeroSelection = () => {
         </Box>
         <Box>
           <Text>Location</Text>
+          <Select placeholder="Select option">
+            <option value="option1">Option 1</option>
+            <option value="option2">Option 2</option>
+            <option value="option3">Option 3</option>
+          </Select>
         </Box>
       </Flex>
       <Flex>
