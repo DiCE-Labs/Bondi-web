c9705ecf8eabdebc4fa6e5fd3310a4f21c9fd78c
tamani phiri <tamanigabriel0@gmail.com>
2024-02-10 23:39:04 +0200

    c

--- src/constants/HeroSelection.tsx
+++ src/constants/HeroSelection.tsx
@@ -1,11 +1,16 @@
-import { Box } from "@chakra-ui/react"
+import { Box, useColorModeValue } from "@chakra-ui/react";
 
 const HeroSelection = () => {
   return (
-    <Box className="md:grid-cols-4 grid grid-cols-1 border-4">
+    <Box
+      className={`md:grid-cols-4 grid grid-cols-1 ${useColorModeValue(
+        "border-[#1B98E0]",
+        "border-[#A2FAA3]"
+      )} border-4`}
+    >
       1234
     </Box>
-  )
-}
+  );
+};
 
-export default HeroSelection
+export default HeroSelection;
