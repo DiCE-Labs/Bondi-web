eca8c92e11e75e379798db7a1079b9b01073be09
tamani phiri <tamanigabriel0@gmail.com>
2024-02-11 00:55:17 +0200

    c

--- src/components/home/Services.tsx
+++ src/components/home/Services.tsx
@@ -2,9 +2,9 @@ import { Box, Text } from "@chakra-ui/react";
 
 const Services = () => {
   return (
-    <Box>
+    <Box className=" mt-8">
       <Text>Our Services</Text>
-      <Box>
+      <Box className=" grid md:grid-cols-3 grid-cols-2 gap-8 md:gap-4 mt-6">
         <Box>
           <Text>The price we offer accordance with the quality we provide</Text>
         </Box>
