5ee1b2be4e967b36ae3a3ed66eb4e5510b5bdf12
tamani phiri <tamanigabriel0@gmail.com>
2024-02-11 00:45:50 +0200

    c

---
+++ src/components/home/Services.tsx
@@ -0,0 +1,22 @@
+import { Box, Text } from "@chakra-ui/react";
+
+const Services = () => {
+  return (
+    <Box>
+      <Text>Our Services</Text>
+      <Box>
+        <Box>
+          <Text>The price we offer accordance with the quality we provide</Text>
+        </Box>
+        <Box>
+          <Text>Official legality in the relevant government</Text>
+        </Box>
+        <Box>
+          <Text>We always work with agents in their fields</Text>
+        </Box>
+      </Box>
+    </Box>
+  );
+};
+
+export default Services;
