c8a0196cff1b6a4d97107de1c7ccae9f070486cf
tamani phiri <tamanigabriel0@gmail.com>
2024-02-14 20:55:24 +0200

    c

--- src/components/home/FeaturedListings.tsx
+++ src/components/home/FeaturedListings.tsx
@@ -1,11 +1,22 @@
-import { Box } from "@chakra-ui/react"
+import { ArrowForwardIcon } from "@chakra-ui/icons";
+import { Box, Flex, Text } from "@chakra-ui/react";
+import { Link } from "react-router-dom";
 
 const FeaturedListings = () => {
   return (
     <Box as="section" className=" min-h-screen w-full">
-      fs
+      <Flex>
+        <Text>Featured Listings</Text>
+        <Link to={"#"} className=" flex items-center gap-2">
+          {" "}
+          <span>Browse All Featured</span>
+          <span>
+            <ArrowForwardIcon />
+          </span>{" "}
+        </Link>
+      </Flex>
     </Box>
-  )
-}
+  );
+};
 
-export default FeaturedListings
+export default FeaturedListings;
