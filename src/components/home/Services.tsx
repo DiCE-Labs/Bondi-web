222d2b5cddd77eabef612d8b2333b6b811214079
tamani phiri <tamanigabriel0@gmail.com>
2024-02-14 20:37:22 +0200

    c

--- src/components/home/Services.tsx
+++ src/components/home/Services.tsx
@@ -1,19 +1,25 @@
-import { Box, Text } from "@chakra-ui/react";
+import { Box, Flex, Text } from "@chakra-ui/react";
+import IconMoneyStack from "../../assets/icons/MoneyStack";
+import IconHandshake from "../../assets/icons/HandShake";
+import IconPeopleGroup from "../../assets/icons/PeopleGroup";
 
 const Services = () => {
   return (
     <Box className=" mt-8">
       <Text className=" font-bold text-xl">Our Services</Text>
       <Box className=" grid md:grid-cols-3 grid-cols-2 gap-8 md:gap-4 mt-6">
-        <Box>
+        <Flex className=" flex-col gap-4">
+          <IconMoneyStack />
           <Text>The price we offer accordance with the quality we provide</Text>
-        </Box>
-        <Box>
+        </Flex>
+        <Flex className=" flex-col gap-4">
+          <IconHandshake />
           <Text>Official legality in the relevant government</Text>
-        </Box>
-        <Box>
+        </Flex>
+        <Flex className=" flex-col gap-4">
+          <IconPeopleGroup />
           <Text>We always work with agents in their fields</Text>
-        </Box>
+        </Flex>
       </Box>
     </Box>
   );
