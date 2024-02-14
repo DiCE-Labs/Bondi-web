7c9f3c6f60ef14b7be52fe4a4cb4d5a2f54c4604
tamani phiri <tamanigabriel0@gmail.com>
2024-02-14 21:02:10 +0200

    c

--- src/components/home/FeaturedListings.tsx
+++ src/components/home/FeaturedListings.tsx
@@ -4,15 +4,19 @@ import { Link } from "react-router-dom";
 
 const FeaturedListings = () => {
   return (
-    <Box as="section" className=" min-h-screen w-full">
-      <Flex className=" w-full justify-between">
-        <Text className=" text-4xl font-bold">Featured Listings</Text>
-        <Link to={"#"} className=" flex items-center gap-2">
+    <Box as="section" className=" min-h-screen w-full py-12">
+      <Flex className=" w-full gap-8 justify-between items-center">
+        <Text className=" text-2xl md:text-4xl font-bold">
+          Featured Listings
+        </Text>
+        <Link to={"#"} className="md:text-base text-sm" >
           {" "}
-          <span>Browse All Featured</span>
-          <span>
-            <ArrowForwardIcon />
-          </span>{" "}
+          <p>
+            Browse All Featured{" "}
+            <span>
+              <ArrowForwardIcon />
+            </span>{" "}
+          </p>
         </Link>
       </Flex>
     </Box>
