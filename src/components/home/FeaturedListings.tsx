c075dd4000622f16389a8d9fdabab5e0a468baf3
tamani phiri <tamanigabriel0@gmail.com>
2024-02-14 20:50:07 +0200

    c

---
+++ src/components/home/FeaturedListings.tsx
@@ -0,0 +1,11 @@
+import { Box } from "@chakra-ui/react"
+
+const FeaturedListings = () => {
+  return (
+    <Box as="section" className=" min-h-screen w-full">
+      fs
+    </Box>
+  )
+}
+
+export default FeaturedListings
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,39 +1,12 @@
-import { Box, Image, Text, useColorModeValue } from "@chakra-ui/react";
-import bg from "../assets/hero.webp";
-import HeroSelection from "../constants/HeroSelection";
-import Services from "../components/home/Services";
+import FeaturedListings from "../components/home/FeaturedListings";
+import Hero from "../components/home/Hero";
 
 const Home = () => {
   return (
-    <Box
-      as="header"
-      className="grid grid-cols-1 lg:grid-cols-3 min-h-screen gap-8 py-12 md:py-0"
-    >
-      <Box
-        className={`flex items-center lg:order-first order-last justify-center w-full ${useColorModeValue(
-          "bg-[#1B98E0]",
-          "bg-[#A2FAA3]"
-        )}`}
-      >
-        <Image
-          src={bg}
-          alt="heroImg"
-          loading="lazy"
-          className=" w-full md:h-full h-96 object-cover grayscale"
-        />
-      </Box>
-      <Box className=" lg:col-span-2 col-span-1 justify-center items-start flex flex-col">
-        <Text className="md:text-7xl text-5xl font-bold">
-          We help people to realize their dream property
-        </Text>
-        <Text className={` max-w-md mt-4 text-gray-500`}>
-          We are creative people who provide the best way to you who want to
-          have a new confortable and suitable place to live
-        </Text>
-        <HeroSelection />
-        <Services />
-      </Box>
-    </Box>
+    <>
+      <Hero />
+      <FeaturedListings/>
+    </>
   );
 };
 
