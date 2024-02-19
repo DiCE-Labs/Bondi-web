b3b79ad6974988c8064dffa887a86225ff279306
tamani phiri <tamanigabriel0@gmail.com>
2024-02-19 21:03:35 +0200

    c

---
+++ src/components/home/BottomHero.tsx
@@ -0,0 +1,12 @@
+import { Box } from "@chakra-ui/react"
+
+const BottomHero = () => {
+  return (
+    <Box className="grid lg:grid-cols-2 grid-cols-1 w-full py-12 lg:order-first order-last"> 
+      <Box></Box>
+      <Box></Box>
+    </Box>
+  )
+}
+
+export default BottomHero
