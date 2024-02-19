6b272b6394eb91c1916d9bd6fd20f5d36132e5c6
tamani phiri <tamanigabriel0@gmail.com>
2024-02-19 21:13:49 +0200

    c

--- src/components/home/BottomHero.tsx
+++ src/components/home/BottomHero.tsx
@@ -4,7 +4,9 @@ const BottomHero = () => {
   return (
     <Box className="grid lg:grid-cols-2 grid-cols-1 w-full py-12 lg:order-first order-last">
       <Box>
-        <Text as={"h1"}>Comfort Is Our Top Priority For You</Text>
+        <Text as={"h1"} className="text-2xl md:text-4xl font-bold">
+          Comfort Is Our Top Priority For You
+        </Text>
         <Text>
           We guarantee that the products we sell will make our customers happy
           because we are very concerned about our consumer satisfaction
