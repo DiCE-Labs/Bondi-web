1fdf5f7c80153e8d3b4ed2b76d54b49e27befd70
tamani phiri <tamanigabriel0@gmail.com>
2024-02-19 18:24:53 +0200

    c

--- src/components/home/FeaturedCategories.tsx
+++ src/components/home/FeaturedCategories.tsx
@@ -13,7 +13,7 @@ const FeaturedCategories = () => {
         <Link to={"#"} className="md:text-base text-sm font-bold">
           {" "}
           <p>
-            Browse All Categories{" "}
+            Browse Categories{" "}
             <span>
               <ArrowForwardIcon />
             </span>{" "}
@@ -22,8 +22,8 @@ const FeaturedCategories = () => {
       </Flex>
       <Box className=" w-full grid lg:grid-cols-3 grid-cols-1 gap-8">
         {categories.map((item, i) => (
-          <Flex key={i} className=" items-center border-lime-400 border w-full border-opacity-40 p-4">
-            <Box>{item.icon}</Box>
+          <Flex key={i} className=" items-center rounded-xl border-lime-400 border w-full border-opacity-40 p-4">
+            <Box className={`bg-lime-400 p-2`}>{item.icon}</Box>
           </Flex>
         ))}
       </Box>
